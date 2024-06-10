e3af07c33a847d29995224f354c1628ed63e827f
Ben Klinovsky <bnk989@gmail.com>
2024-06-10 12:36:04 +0300

    styled the header on mobile

--- components/Header.vue
+++ components/Header.vue
@@ -1,24 +1,13 @@
 <template>
     <header>
         <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800 shadow">
-            <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+            <div class="relative flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                 <NuxtLink to="/" class="flex items-center">
                     <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Associate Logo" />
                     <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Associate</span>
                 </NuxtLink>
                 <div class="flex items-center lg:order-2">
                     <!-- <div v-if="!user" class="flex items-center lg:order-2"> -->
-                    <div class="flex items-center lg:order-2">
-                        <NuxtLink
-                            to="/login"
-                            class=" hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
-                            >Log in</NuxtLink>
-                        <NuxtLink
-                            to="/login"
-                            class="bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
-                            >Get started</NuxtLink
-                        >
-                    </div>
                     <!-- <div v-else>
                         <UserAvatarMenu />
                     </div> -->
@@ -48,8 +37,8 @@
                         </svg>
                     </button>
                 </div>
-                <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
-                    <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                <div class="hidden absolute md:static md:bg-opacity-0 z-10 bg-blue-950 top-12 justify-between items-center w-dvw -left-4 lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
+                    <ul class="flex flex-col md:items-center mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                         <li v-for="link in menu">
                             <NuxtLink
                                 :to="link.url"
@@ -58,6 +47,18 @@
                                 >{{link.name}}</NuxtLink
                             >
                         </li>
+
+                    <div class="flex items-center justify-evenly bg-blue-200 bg-opacity-20 my-2 md:bg-opacity-0 md:my-0 ">
+                        <NuxtLink
+                            to="/login"
+                            class=" hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
+                            >Log in</NuxtLink>
+                        <NuxtLink
+                            to="/login"
+                            class="bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
+                            >Get started</NuxtLink
+                        >
+                    </div>
                     <button 
                     class="m-4 md: my-0"
                     @click="colorMode.preference === 'light' ? colorMode.preference = 'dark' : colorMode.preference = 'light' " >
