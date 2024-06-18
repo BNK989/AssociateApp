f00b4751d10d3695d359d89d485d7e87e96f404d
Ben Klinovsky <bnk989@gmail.com>
2024-06-18 13:15:02 +0300

    Add basic about and contact pages

---
+++ pages/about.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1 class="text-3xl font-bold">Lets check how well you now each other</h1>
+        <h3>This is the about page</h3>
+        <p></p>
+    </div>
+</template>
