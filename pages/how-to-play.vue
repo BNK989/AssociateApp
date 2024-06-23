6380878e2052b6a1b8ad5db830609409cf1ee1fd
Ben Klinovsky <bnk989@gmail.com>
2024-06-23 15:44:48 +0300

    Add how to play page

---
+++ pages/how-to-play.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="p-4">
+        <h1 class="text-3xl font-bold mb-4">How To Play</h1>
+        <h3>Welcome to the game here is a quick stating guide</h3>
+        <p
+            class="py-2 first-letter:text-4xl">
+            To start the game first you need to start a chat, you can either
+            create a new chat or join an existing one (if you've played before)
+        </p>
+        <p
+            class="py-2 ">
+            After creating the game go ahead and add players or play by your-self if you want, the first part is just to add words, go ahead and type the first thing that comes to mind.
+        </p>
+        <p
+            class="py-2 ">
+            When you feel like you have enough words in the game you can switch to guessing mode, now you have to think backwords! and try to guess each word going back to the original word
+        </p>
+        <p
+            class="py-2 ">
+            The aim of the game is to guess the word in as few attempts as possible
+        </p>
+        <h5
+            class="py-2 text-2xl">
+            Happy playing!
+        </h5>
+    </div>
+</template>
