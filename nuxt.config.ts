6cb713d718343a62adc3bfcb4f09f88ee591127d
Ben Klinovsky <bnk989@gmail.com>
2024-06-23 17:38:57 +0300

    Prepare for i18 integration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,27 +1,63 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ['~/assets/css/main.css'],
-  app: {
-    head: {
-      title: 'Associate Game',
-      meta: [
-        // { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'A fun chat like association game for one or more players', name: 'Give it a shot play now!', content: 'My awesome app' }
-      ],
-    }
-  },
-  
-  devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode', '@nuxtjs/supabase', '@vueuse/nuxt', // {
-  //   autoImports: ['defineStore', 'acceptHMRUpdate']
-  // },
-  '@pinia/nuxt', "@nuxt/image"],
-  supabase: {
-    // Options
-    redirect: false,
-  },
-  imports: {
-    dirs: ['stores'],
-  },
-  
+    css: ['~/assets/css/main.css'],
+    app: {
+        head: {
+            title: 'Associate Game',
+            meta: [
+                // { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+                {
+                    hid: 'A fun chat like association game for one or more players',
+                    name: 'Give it a shot play now!',
+                    content: 'My awesome app',
+                },
+            ],
+        },
+    },
+
+    devtools: { enabled: true },
+    modules: [
+        '@nuxtjs/tailwindcss',
+        '@nuxtjs/color-mode',
+        '@nuxtjs/supabase', // {
+        '@vueuse/nuxt', //   autoImports: ['defineStore', 'acceptHMRUpdate']
+        // },
+        '@pinia/nuxt',
+        '@nuxt/image',
+        '@nuxtjs/i18n',
+    ],
+    supabase: {
+        // Options
+        redirect: false,
+    },
+    imports: {
+        dirs: ['stores'],
+    },
+    //@ts-ignore
+    i18n: {
+        strategy: 'prefix_except_default',
+        defaultLocale: 'en',
+        lazy: true,
+        langDir: 'locales',
+        locales: [
+            {
+                code: 'en',
+                iso: 'en-US',
+                name: 'English',
+                file: 'en-US.json',
+            },
+            {
+                code: 'es',
+                iso: 'es-ES',
+                name: 'Español',
+                file: 'es-ES.json',
+            },
+            {
+                code: 'he',
+                iso: 'he-IL',
+                name: 'Hebrew',
+                file: 'he-IL.json',
+            },
+        ],
+    },
 })
--- pages/about.vue
+++ pages/about.vue
@@ -2,6 +2,23 @@
     <div>
         <h1 class="text-3xl font-bold">Lets check how well you now each other</h1>
         <h3>This is the about page</h3>
-        <p></p>
+        <!-- <pre>{{ locale }}</pre>
+        <pre>{{ locales[0] }}</pre>
+        <pre>{{ $t('About_title') }}</pre>
+        <div class="flex gap-4">
+        </div>
+        <select v-model="selectedLocale">
+            <option v-for="l in locales" :key="l.code" :value="l.code">{{ l.name }}</option>
+        </select>
+        <nuxt-link :to="localPath('/')">Go to homepage</nuxt-link> -->
     </div>
 </template>
+
+<script lang="ts" setup>
+const {locale, setLocale, locales} = useI18n()
+const localPath = useLocalePath()
+const selectedLocale = computed({
+    get: () => locale.value,
+    set: (v) => setLocale(v)
+})
+</script>
