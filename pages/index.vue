354ac73e6e6fb948a64304c4bdd3fba7eba424c2
Ben Klinovsky <bnk989@gmail.com>
2024-07-01 17:14:39 +0300

    improved index page

--- pages/index.vue
+++ pages/index.vue
@@ -4,23 +4,33 @@
             {{ $t('Welcome') }}
         </h1>
         <button
+            v-if="storeUser"
             @click="createNewGame"
             class="py-2 px-3 my-4 rounded-full w-full md:w-2/5 bg-accent-3">
             {{ $t('Start_New_Game_Btn') }}
         </button>
-        <ul class="grid gap-4 grid-cols-2 md:grid-cols-4">
-            <li v-for="game in activeGames">
+        <nuxt-link v-else :to="localPath('/profile/login?signup=true')">
+            <button
+                class="py-2 px-3 my-4 rounded-full w-full md:w-2/5 bg-accent-3">
+                {{ $t('Signup_to_play') }}
+            </button>
+        </nuxt-link>
+        <!-- <pre>{{ activeGames }}</pre> -->
+        <h2 class="text-2xl my-2">{{ $t('Active_Games') }}</h2>
+
+        <ul class="flex gap-4 flex-wrap w-full">
+            <li v-for="game in activeGames" class="">
                 <nuxt-link :to="localPath(`/game/${game.id}`)">
                     <div
-                        class="grid grid-cols-[minmax(min-content,1fr)_min-content] gap-3 rounded bg-accent-3/80 md:max-w-56 p-3 cursor-pointer hover:bg-accent-3/50 duration-300">
+                        class="grid grid-cols-[minmax(min-content,1fr)_min-content] gap-3 min-h-32 rounded bg-accent-3/80 md:max-w-56 p-3 cursor-pointer hover:bg-accent-3/50 duration-300">
                         <h2 class="text-2xl">{{ game.title }}</h2>
                         <h6 class="text-sm lowercase self-start">
                             {{ game.GameMode }}
                         </h6>
-                        <ul class="col-span-full flex gap-2 flex-wrap">
+                        <ul class="col-span-full flex gap-2 flex-wrap self-end">
                             <li
                                 v-for="p in game.Users"
-                                class="bg-accent-2/30 px-2 rounded-full">
+                                class="bg-accent-2/30 px-2 rounded-full flex-center max-h-6">
                                 {{
                                     p.user.email === email
                                         ? 'You'
@@ -32,6 +42,39 @@
                 </nuxt-link>
             </li>
         </ul>
+        <!-- END OF ACTIVE GAME -->
+        <div v-if="otherGames.length > 0">
+            <h2 class="text-2xl my-2">
+                {{ $t('Non_Active_Games') }}
+            </h2>
+
+            <ul class="grid gap-4 grid-cols-2 md:grid-cols-4">
+                <li v-for="game in otherGames">
+                    <nuxt-link :to="localPath(`/game/${game.id}`)">
+                        <div
+                            class="grid grid-cols-[minmax(min-content,1fr)_min-content] gap-3 rounded bg-accent-3/80 md:max-w-56 p-3 cursor-pointer hover:bg-accent-3/50 duration-300">
+                            <h2 class="text-2xl">{{ game.title }}</h2>
+                            <h6 class="text-sm lowercase self-start">
+                                {{ game.GameMode }}
+                            </h6>
+                            <small>{{ game.status }}</small>
+                            <ul class="col-span-full flex gap-2 flex-wrap">
+                                <li
+                                    v-for="p in game.Users"
+                                    class="bg-accent-2/30 px-2 rounded-full">
+                                    {{
+                                        p.user.email === email
+                                            ? 'You'
+                                            : p.user.userName
+                                    }}
+                                </li>
+                            </ul>
+                        </div>
+                    </nuxt-link>
+                </li>
+            </ul>
+        </div>
+        <!-- END OF NON ACTIVE GAMES -->
     </div>
 </template>
 
@@ -48,8 +91,15 @@ const router = useRouter()
 const user = useSupabaseUser()
 
 const email = user?.value?.email
-const activeGames = ref([])
+const allGames = ref([])
+//@ts-ignore
 const myGames = computed(() => storeUser.value?.games?.join(',') || [])
+const activeGames = computed(
+    () => allGames.value?.filter((g) => g.status === 'ACTIVE') || [],
+)
+const otherGames = computed(
+    () => allGames.value?.filter((g) => g.status !== 'ACTIVE') || [],
+)
 
 const getGamesUpdate = () => {
     realtimeChannel = supabase.channel('public:Games').on(
@@ -82,9 +132,9 @@ watch(() => myGames.value, getGamesUpdate)
 
 async function getActiveGames() {
     // console.log('storeUser.value?.id:', storeUser.value?.id)
-    if (!storeUser.value?.id) activeGames.value = []
+    if (!storeUser.value?.id) allGames.value = []
     else
-        activeGames.value = await $fetch(
+        allGames.value = await $fetch(
             `/api/all-games?user_id=${storeUser.value?.id}`,
         )
 }
