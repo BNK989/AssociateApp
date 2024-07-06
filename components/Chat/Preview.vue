ab83ae621d2ec7464155d428b15f154b27c03c79
Ben Klinovsky <bnk989@gmail.com>
2024-07-06 11:56:57 +0300

    improved css for index page

--- components/Chat/Preview.vue
+++ components/Chat/Preview.vue
@@ -1,29 +1,25 @@
 <template>
-    <li>
+    <li
+        class="flex-1"
+        :class="game.GameMode === 'SOLVE' ? 'bg-accent-2/40' : 'bg-accent-3/60'">
         <NuxtLink :to="localPath(`/game/${game.id}`)">
             <div
-                class="grid grid-cols-[minmax(min-content,1fr)_min-content] gap-1 md:min-w-48 min-h-36 max-h-fit rounded bg-accent-3/80 p-2 cursor-pointer hover:bg-accent-3/50 duration-300">
+                class="grid grid-cols-[minmax(min-content,1fr)_min-content] gap-1 md:min-w-48 min-h-36 max-h-fit rounded p-2 cursor-pointer hover:bg-accent-3/50 duration-300">
                 <h2 class="text-2xl">{{ game.title }}</h2>
                 <h6 class="text-sm lowercase self-start">
                     {{ game.GameMode }}
                 </h6>
-                <h4
-                    v-if="game._count.messages"
-                    class="col-span-full text-content/75">
+                <h4 v-if="game._count.messages" class="col-span-full text-content/75">
                     {{ game._count.messages }} Messages
                 </h4>
-                <h4
-                    v-if="game.updatedAt"
-                    class="col-span-full text-content/75 text-sm">
+                <h4 v-if="game.updatedAt" class="col-span-full text-content/75 text-sm">
                     {{ relativeTime(game.updatedAt) }}
                 </h4>
                 <ul class="col-span-full flex gap-2 flex-wrap self-end">
                     <li
                         v-for="p in game.Users"
                         class="bg-accent-2/30 px-2 rounded-full flex-center max-h-6">
-                        {{
-                            p.user.email === userEmail ? 'You' : p.user.userName
-                        }}
+                        {{ p.user.email === userEmail ? 'You' : p.user.userName }}
                     </li>
                 </ul>
             </div>
