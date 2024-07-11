c4660eca41401f9c0ace7c03459c2c981438e384
Ben Klinovsky <bnk989@gmail.com>
2024-07-11 08:57:13 +0300

    testing api

--- server/api/ai/test.get.js
+++ server/api/ai/test.get.js
@@ -8,6 +8,13 @@ export default defineEventHandler(async (e) => {
             gameId: 1,
         },
     })
+    await $fetch('https://webhook.site/9391cac8-91a2-41f0-be02-cb563a31b353', {
+        method: 'POST',
+        body: {
+            gameId: 1,
+            res,
+        },
+    })
 
     return res
 })
