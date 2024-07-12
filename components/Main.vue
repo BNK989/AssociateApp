8fc884a66f574c2997987310d1be661f5e760d9c
Ben Klinovsky <bnk989@gmail.com>
2024-07-12 10:21:19 +0300

    debugging - removed div from layout

--- components/Main.vue
+++ components/Main.vue
@@ -1,7 +1,7 @@
 <template>
     <!-- <pre @click="playSound('nudge')">test</pre> -->
     <div
-        class="main flex-1 backdrop-blur-lg backdrop-saturate-150 md:border rounded md:border-accent-3/25 flex flex-col">
+        class="main overflow-y-scroll backdrop-blur-lg backdrop-saturate-150 md:border rounded md:border-accent-3/25 flex flex-col">
         <ChatTop
             :gameMode="gameMode"
             :wordLength="messages?.length || 0"
